3901f5f9ca187f7bbe5ed6104a3e1f1c98365981
Mariusz Nowak <mariusz@medikoo.com>
2013-10-11 18:27:24 +0200

    Test context

--- test/for-of.js
+++ test/for-of.js
@@ -5,12 +5,14 @@ var ArrayIterator = require('../array')
   , slice = Array.prototype.slice;
 
 module.exports = function (t, a) {
-	var i = 0, x = ['raz', 'dwa', 'trzy'];
+	var i = 0, x = ['raz', 'dwa', 'trzy'], y = {};
 	t(x, function () {
-		a.deep(slice.call(arguments), [x[i]], "Array " + (i++) + "#");
-	});
+		a.deep(slice.call(arguments), [x[i]], "Array " + i + "#");
+		a(this, y, "Array: context:  " + (i++) + "#");
+	}, y);
 	i = 0;
 	t(new ArrayIterator(x), function () {
-		a.deep(slice.call(arguments), [x[i]], "Array " + (i++) + "#");
-	});
+		a.deep(slice.call(arguments), [x[i]], "Iterator " + i + "#");
+		a(this, y, "Iterator: context:  " + (i++) + "#");
+	}, y);
 };
