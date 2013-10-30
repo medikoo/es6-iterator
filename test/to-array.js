93efe5cedf3c1b0c605cc06e92045a76eee06899
Mariusz Nowak <mariusz@medikoo.com>
2013-10-30 13:21:11 +0100

    Dedicated string handling in `toArray`

--- test/to-array.js
+++ test/to-array.js
@@ -5,5 +5,6 @@ var ArrayIterator = require('../array');
 module.exports = function (t, a) {
 	var x = ['raz', 'dwa', 'trzy'];
 	a(t(x), x, "Array");
-	a.deep(t(new ArrayIterator(x)), ['raz', 'dwa', 'trzy']);
+	a.deep(t(new ArrayIterator(x)), ['raz', 'dwa', 'trzy'], "Iterator");
+	a.deep(t('foo'), ['f', 'o', 'o'], "String");
 };
--- to-array.js
+++ to-array.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var value = require('es5-ext/object/valid-value')
-  , get   = require('./get')
+var value    = require('es5-ext/object/valid-value')
+  , isString = require('es5-ext/string/is-string')
+  , get      = require('./get')
 
-  , isArray  = Array.isArray;
+  , isArray  = Array.isArray, slice = Array.prototype.slice;
 
 module.exports = function (iterable) {
 	var result, arr, iterator;
 	if (isArray(iterable)) return iterable;
+	if (isString(iterable)) return slice.call(iterable);
 	iterator = get(iterable);
 	arr = [];
 	result = value(iterator.next());
