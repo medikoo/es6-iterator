c2fab87888422446e6172bdc021b5b309e8645ee
Mariusz Nowak <mariusz@medikoo.com>
2013-10-30 13:20:50 +0100

    String support in `get`

--- get.js
+++ get.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var ArrayIterator = require('./array')
-  , iterable      = require('./valid-iterable');
+var ArrayIterator  = require('./array')
+  , StringIterator = require('./string')
+  , iterable       = require('./valid-iterable')
+
+  , isArray = Array.isArray;
 
 module.exports = function (obj) {
 	if (typeof iterable(obj)['@@iterator'] === 'function') {
 		return obj['@@iterator']();
 	}
-	return new ArrayIterator(obj);
+	if (isArray(obj)) return new ArrayIterator(obj);
+	return new StringIterator(obj);
 };
--- test/get.js
+++ test/get.js
@@ -8,5 +8,6 @@ module.exports = function (t, a) {
 	a.throws(function () { t({ length: 0 }); }, TypeError, "Array-like");
 	a(t({ '@@iterator': function () { return new Iterator([]); } })
 		instanceof Iterator, true, "Iterator");
-	a(String(t([])), '[object Array Iterator]', 'Array');
+	a(String(t([])), '[object Array Iterator]', " Array");
+	a(String(t('foo')), '[object String Iterator]', "String");
 };
