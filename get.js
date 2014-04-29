16ed4efba36c1a5759db46693f8978dacfd527b3
Mariusz Nowak <mariusz@medikoo.com>
2014-04-29 12:35:35 +0200

    Assure `get` returns proper iterator

--- get.js
+++ get.js
@@ -1,16 +1,13 @@
 'use strict';
 
-var ArrayIterator  = require('./array')
+var isString = require('es5-ext/string/is-string')
+  , ArrayIterator  = require('./array')
   , StringIterator = require('./string')
   , iterable       = require('./valid-iterable')
-  , iteratorSymbol = require('es6-symbol').iterator
-
-  , isArray = Array.isArray;
+  , iteratorSymbol = require('es6-symbol').iterator;
 
 module.exports = function (obj) {
-	if (typeof iterable(obj)[iteratorSymbol] === 'function') {
-		return obj[iteratorSymbol]();
-	}
-	if (isArray(obj)) return new ArrayIterator(obj);
-	return new StringIterator(obj);
+	if (typeof iterable(obj)[iteratorSymbol] === 'function') return obj[iteratorSymbol]();
+	if (isString(obj)) return new StringIterator(obj);
+	return new ArrayIterator(obj);
 };
