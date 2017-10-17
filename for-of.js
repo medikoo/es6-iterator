6cffc333e09bb3171d406ab83cc4d2dac02d39a4
Mariusz Nowak <medyk@medikoo.com>
2017-10-17 12:31:07 +0200

    style: --prettier

--- for-of.js
+++ for-of.js
@@ -3,12 +3,11 @@
 var isArguments = require("es5-ext/function/is-arguments")
   , callable    = require("es5-ext/object/valid-callable")
   , isString    = require("es5-ext/string/is-string")
-  , get         = require("./get")
+  , get         = require("./get");
 
-  , isArray = Array.isArray, call = Function.prototype.call
-  , some = Array.prototype.some;
+var isArray = Array.isArray, call = Function.prototype.call, some = Array.prototype.some;
 
-module.exports = function (iterable, cb/*, thisArg*/) {
+module.exports = function (iterable, cb /*, thisArg*/) {
 	var mode, thisArg = arguments[2], result, doBreak, broken, i, l, char, code;
 	if (isArray(iterable) || isArguments(iterable)) mode = "array";
 	else if (isString(iterable)) mode = "string";
@@ -16,8 +15,8 @@ module.exports = function (iterable, cb/*, thisArg*/) {
 
 	callable(cb);
 	doBreak = function () {
- broken = true;
-};
+		broken = true;
+	};
 	if (mode === "array") {
 		some.call(iterable, function (value) {
 			call.call(cb, thisArg, value, doBreak);
@@ -29,9 +28,9 @@ module.exports = function (iterable, cb/*, thisArg*/) {
 		l = iterable.length;
 		for (i = 0; i < l; ++i) {
 			char = iterable[i];
-			if ((i + 1) < l) {
+			if (i + 1 < l) {
 				code = char.charCodeAt(0);
-				if ((code >= 0xD800) && (code <= 0xDBFF)) char += iterable[++i];
+				if (code >= 0xd800 && code <= 0xdbff) char += iterable[++i];
 			}
 			call.call(cb, thisArg, char, doBreak);
 			if (broken) break;
